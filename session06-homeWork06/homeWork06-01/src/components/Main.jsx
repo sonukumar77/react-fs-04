e32b67ce5afd58b716c400e26f6f8b4bdef188bf
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-04-25 08:02:32 +0530

    Create Main.jsx

---
+++ session06-homeWork06/homeWork06-01/src/components/Main.jsx
@@ -0,0 +1,24 @@
+import "./Main.css";
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div className="main-left">
+        <h1>Header</h1>
+        <h1>Footer</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+          deleniti amet fugit molestias non iusto iste fugiat labore, laborum
+          ad.
+        </p>
+      </div>
+      <div className="main-right">
+        <img
+          src="https://cdn.pixabay.com/photo/2023/03/09/02/53/girl-7839121__340.jpg"
+          alt="poster"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
