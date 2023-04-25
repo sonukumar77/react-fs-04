144e2d5b18fe38df1d67422c4f3bee308e56d07a
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-04-25 07:43:56 +0530

    Create App.js

---
+++ session06-homeWork06/homeWork06-01/src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
