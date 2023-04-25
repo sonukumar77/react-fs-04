1c8a64ac6ea75b36061fbb2a14aff88126c0ccee
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2023-04-25 07:46:35 +0530

    Create Footer.jsx

---
+++ session06-homeWork06/homeWork06-01/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-left">
+        <h3>Block Header</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="footer-middle">
+        <h3>Block Header</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="footer-right">
+        <h3>Block Header</h3>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
